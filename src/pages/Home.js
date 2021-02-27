d59916970c5d4984f98659097c2b27bb4bbbaf47
VAIBHAV <vaibhav@pop-os.localdomain>
2021-02-27 12:22:34 +0530

    fix to errors in filter movie when there are are no filtered results

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-// import Axios from 'axios'
+import Axios from 'axios'
 
 //import components
 import MovieSearch from '../components/MovieSearch'
@@ -56,92 +56,42 @@ const Home = () => {
     const getMovieCollection = () => {
         const data = { label: 'movie' };
         const headers = {
-            Accept: "application/json, text/plain, */*",
+            Accept: "application/json",
+            "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
             "Content-Type": "application/json",
-            // "Access-Control-Allow-Methods": "POST",
-            "Access-Control-Allow-Origin": "*",
+            // "Access-Control-Allow-Methods": "GET, POST",
+            // "Access-Control-Allow-Origin": "*",
         };
-        // Axios({
-        //     method: "post",
-        //     url: `https://crossorigin.me/${URL}`,
-        //     headers: headers,
-        //     data: data,
-        //     mode: "cors"
-        //     // headers: {
-        //     //     "Access-Control-Allow-Origin": "http://todo.naikvaibhav.online",
-        //     //     'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
-        //     // },
-        //     // mode: "cors"
-        //   }).then(response=>console.log(response)).catch(err=> console.log(err))
+        Axios({
+            method: "post",
+            url: `${URL}`,
+            headers: headers,
+            data: data,
+            // mode: "cors"
+          }).then(response=>console.log(response)).catch(err=> console.log(err))
 
 
-          const proxyurl = "https://cors-anywhere.herokuapp.com/";
-          let req = new Request(URL, {
+        let req = new Request(URL, {
             method: 'POST',
-            body: JSON.stringify(data)
-          });
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: data,
+            mode: 'no-cors'
+        });
 
-          fetch(req)
-          .then(response => response.json())
-          .then((contents) => {
-            console.log(JSON.parse(contents))
-          })
-          .catch(() => console.log("Can’t access " + " response. Blocked by browser?"))
-    //     const requestOptions = {
-    //         method: 'POST',
-    //         headers: {
-    //             'Accept': 'application/json',
-    //             'Content-Type': 'application/json'
-    //         },
-    //         mode: 'no-cors',
-    //         body: JSON.stringify(data)
-    //     };
-
-    //     fetch(`${URL}`, requestOptions).then((response) => response.json())
-    //     .then((responseJson) => {
-    //             console.log(responseJson);
-    //             return responseJson
-    //  });
+        fetch(req)
+        .then(response => response.json())
+        .then((contents) => {
+            console.log(contents)
+        })
+        .catch((err) => console.log("Can’t access " + " response. Blocked by browser?", err))
     }
 
     useEffect(()=> {
-        // getMovieCollection()
-        const data = {
-            "list": [
-                {
-                    "title": "Avengers: Endgame",
-                    "duration": 135,
-                    "genre": "Action"
-                },
-                {
-                    "title": "After",
-                    "duration": 150,
-                    "genre": "Drama"
-                },
-                {
-                    "title": "The Hole in the Ground",
-                    "duration": 125,
-                    "genre": "Horror"
-                },
-                {
-                    "title": "Dragon: The hidden world",
-                    "duration": 185,
-                    "genre": "Animation"
-                },
-                {
-                    "title": "Hellboy",
-                    "duration": 115,
-                    "genre": "Thriller"
-                },
-                {
-                    "title": "A star is born",
-                    "duration": 135,
-                    "genre": "Fantasy"
-                }
-            ],
-            "createdAt": "2021-02-25T12:25:25.921Z",
-            "updatedAt": "2021-02-25T12:25:25.921Z"
-        }
+        //CORS error hence used the api data in state
+        getMovieCollection()
     },[])
 
     const getSearchText = (searchValue) => {
@@ -163,7 +113,7 @@ const Home = () => {
         setSearchedMovieList([])
         const tempCollection = genreSelected !== genreList[0] ? movieList.filter(movie => movie.genre.toLowerCase() === genreSelected.toLowerCase()) : movieList
         setFilteredMovieList([...tempCollection])
-        setFilterTabClicked(!filterTabClicked)
+        setFilterTabClicked(true)
         setMovieCount(tempCollection.length)
     }
 
